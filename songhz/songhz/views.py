a16844af0996e9102c0778d5a5d35e4e586fd066
songhz <shzubuntu@163.com>
2019-06-29 21:17:44 +0800

    ui

--- songhz/songhz/views.py
+++ songhz/songhz/views.py
@@ -18,6 +18,105 @@ def hello(request):
     context['hello'] = 'Hello World!aaaa'
     return render(request, 'hello.html', context)
 
+def test(request):
+    context          = {}
+    context['hello'] = 'Hello World!aaaa'
+    return render(request, 'index.html', context)
+def table(request):
+    context          = {}
+    context['hello'] = 'Hello World!aaaa'
+    return render(request, 'tables.html', context)
+
+def calendar(request):
+    context          = {}
+    context['hello'] = 'Hello World!aaaa'
+    return render(request, 'calendar.html', context)
+
+def buttons(request):
+    context          = {}
+    context['hello'] = 'Hello World!aaaa'
+    return render(request, 'buttons.html', context)
+
+def interface(request):
+    context          = {}
+    context['hello'] = 'Hello World!aaaa'
+    return render(request, 'interface.html', context)
+
+def editors(request):
+    context          = {}
+    context['hello'] = 'Hello World!aaaa'
+    return render(request, 'editors.html', context)
+
+def stats(request):
+    context          = {}
+    context['hello'] = 'Hello World!aaaa'
+    return render(request, 'stats.html', context)
+
+def form(request):
+    context          = {}
+    context['hello'] = 'Hello World!aaaa'
+    return render(request, 'form.html', context)
+
+def home(request):
+    context = {}
+    ansible = MyRunner('config/hosts')
+    ansible.run('all', 'shell', "hostname")
+    # 结果
+    result = ansible.get_result()
+
+    #print json.dumps(result,indent=4)
+    hostlist=[]
+    print(type(result))
+    print result['success'].keys()
+    for host in result['success'].keys():
+        res = result['success'][host]['stdout']
+        hostlist.append((host,res))
+        isexist = Host.objects.filter(ip=host)
+        if(len(isexist)>=1):
+            continue
+        test1 = Host(hostname=res, ip=host)
+        test1.save()
+
+    #print(a)
+    context['result'] = hostlist
+    return render(request, 'hello.html', context)
+
+def gethost(request):
+    context = {}
+    hostlist = Host.objects.all()
+    host_list = []
+    for host in hostlist:
+        host_list.append((host.hostname,host.ip))
+    context['result'] = json.dumps(host_list)
+    return render(request, 'hello.html', context)
+
+def getip(request):
+    context = {}
+    hostlist = Ip.objects.all()
+    host_list = []
+    for host in hostlist:
+        host_list.append((host.ip,host.status,host.comment))
+    context['result'] = json.dumps(host_list)
+    return render(request, 'hello.html', context)
+#扫描可用IP
+def scan(request):
+    context= {}
+    net = "192.168.31.0/24"
+    network = Network(net);
+    hosts_up_list = network.get_hosts_up()
+    for host in hosts_up_list:
+        isexist = Ip.objects.filter(ip=host)
+        if(len(isexist)>0):
+            if  isexist[0].status=='used':
+                continue
+            else:
+                Ip.objects.filter(ip=host).update(status='used')
+
+def form(request):
+    context          = {}
+    context['hello'] = 'Hello World!aaaa'
+    return render(request, 'form.html', context)
+
 def home(request):
     context = {}
     ansible = MyRunner('config/hosts')
